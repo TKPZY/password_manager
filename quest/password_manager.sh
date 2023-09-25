5e5856c8e4149c46302f0edca5535eecdcc97b04
TKPZY <toyoharakazuki@gmail.com>
2023-09-25 21:50:39 +0900

    step2

--- quest/password_manager.sh
+++ quest/password_manager.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
-echo "パスワードマネージャーへようこそ!"
-echo "次の選択肢から入力してください(Add Password/Get Password/Exit):"
-read choices
+while true; do
+    echo "パスワードマネージャーへようこそ!"
+    echo "次の選択肢から入力してください(Add Password/Get Password/Exit):"
+    read choices
 
-case $choices in
-    "Add Password")
-	read -p "サービス名を入力してください:" service
-	read -p "ユーザー名を入力してください:" user
-	read -p -s "パスワードを入力してください:" password
-	echo "$service:$user:$password" >> passwords.md
-	echo "パスワードの追加は成功しました。"
-	;;
-    "Get Password")
-	read -p "サービス名を入力してください:" service
-	password=$(grep "$service:" passwords.md | cut -d: -f3)
-	if [ -z "$password"]; then
-	    echo "そのサービスは登録されていません。"
-	else
-	    echo "サービス名:$service"
-	    echo "ユーザー名:$(grep "$service:" passwords.md | cut -d: -f2)"
-	    echo "パスワード:$password"
-	fi
-	;;
-    "Exit")
-	echo "Thank you\033[31m! \033[m"
-	;;
-    *)
-	echo "入力が間違えています。Add Password/Get Password/Exitから入力してください。"
-	;;
-esac
+    case $choices in
+	"Add Password")
+	    read -p "サービス名を入力してください:" service
+	    read -p "ユーザー名を入力してください:" user
+	    read -s -p "パスワードを入力してください:" password
+	    echo "$service:$user:$password" >> passwords.md
+	    echo "パスワードの追加は成功しました。"
+	    ;;	    
+	"Get Password")
+	    read -p "サービス名を入力してください:" service
+	    password=$(grep "$service:" passwords.md | cut -d: -f3)
+	    if [ -z "$password" ]; then
+		echo "そのサービスは登録されていません。"
+	    else
+		echo "サービス名:$service"
+		echo "ユーザー名:$(grep "$service:" passwords.md | cut -d: -f2)"
+		echo "パスワード:$password"
+	    fi
+	    ;;
+	"Exit")
+	    echo -e "Thank you\033[31m! \033[m"
+	    exit
+	    ;;
+	*)
+	    echo "入力が間違えています。Add Password/Get Password/Exitから入力してください。"
+	    ;;
+    esac
+done
